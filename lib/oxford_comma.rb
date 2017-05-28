4ebc529851d92b9777abafcfcc2a76da910dc44c
Paul Yarabe <pyarabe@mba2018.hbs.edu>
2017-05-28 01:57:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.length == 1 || array.length == 2
+    array.join(" and ")
+  else
+    last = array.pop # returns 'starfruit'
+    array.join(", ") + ", and " + last
+  end
 end
